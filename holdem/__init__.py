5700a7caea6a2de326889b0d25e9c9169d7ed4c4
act65 <te1farac@gmail.com>
2018-04-22 08:28:35 +1200

    added action and bid error classes

--- holdem/__init__.py
+++ holdem/__init__.py
@@ -3,6 +3,7 @@ from gym.envs.registration import register
 
 from .env import TexasHoldemEnv
 from .utils import card_to_str, hand_to_str, safe_actions, action_table
+from .player import ActionError, BidError
 
 register(
  	id='TexasHoldem-v0',
--- holdem/player.py
+++ holdem/player.py
@@ -26,6 +26,19 @@ from gym import error
 
 from treys import Card
 
+class ActionError(Exception):
+    # should have called, but passed
+    # ?
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+
+class BidError(Exception):
+    # you dont have that much money
+    # the bid doesnt meet the required raise
+    # ?
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+
 
 class Player(object):
 
@@ -43,6 +56,7 @@ class Player(object):
     self.lastsidepot = 0
     self._seat = -1
     self.handrank = -1
+    self.errors = 0
 
     # flags for table management
     self.emptyplayer = emptyplayer
@@ -89,6 +103,7 @@ class Player(object):
     self.stack = table_state.get('stack')
     self.hand = table_state.get('pocket_cards')
 
+
   # cleanup
   def player_move(self, table_state, action):
     self.update_localstate(table_state)
@@ -100,31 +115,40 @@ class Player(object):
     raise_amount = int(raise_amount)
     action_idx = int(action_idx)
 
-    if tocall == 0:
-      assert action_idx in [Player.CHECK, Player.RAISE]
+    if (tocall == 0) and (action_idx!=Player.FOLD):
+      if action_idx not in [Player.CHECK, Player.RAISE]:
+          self.errors -= 1
+          raise ActionError('player {}: action must be check (0) or raise (2) but was {}'.format(self.player_id, action_idx))
       if action_idx == Player.RAISE:
         if raise_amount < minraise:
-          raise error.Error('raise must be greater than minraise {}'.format(minraise))
+          self.errors -= 1
+          raise BidError('player {}: raise must be greater than minraise {}'.format(self.player_id, minraise))
         if raise_amount > self.stack:
-          raise error.Error('raise must be less than maxraise {}'.format(self.stack))
+          self.errors -= 1
+          raise BidError('player {}: raise must be less than maxraise {}'.format(self.player_id, self.stack))
         move_tuple = ('raise', raise_amount)
       elif action_idx == Player.CHECK:
         move_tuple = ('check', 0)
       else:
-        raise error.Error('invalid action ({}) must be check (0) or raise (2)'.format(action_idx))
+        self.errors -= 1
+        raise ActionError('player {}: invalid action ({}) must be check (0) or raise (2)'.format(self.player_id, action_idx))
     else:
       if action_idx not in [Player.RAISE, Player.CALL, Player.FOLD]:
-        raise error.Error('invalid action ({}) must be raise (2), call (1), or fold (3)'.format(action_idx))
+        self.errors -= 1
+        raise ActionError('player {}: invalid action ({}) must be raise (2), call (1), or fold (3)'.format(self.player_id, action_idx))
       if action_idx == Player.RAISE:
         if raise_amount < minraise:
-          raise error.Error('raise must be greater than minraise {}'.format(minraise))
+          self.errors -= 1
+          raise BidError('player {}: raise must be greater than minraise {}'.format(self.player_id, minraise))
         if raise_amount > self.stack:
-          raise error.Error('raise must be less than maxraise {}'.format(self.stack))
+          self.errors -= 1
+          raise BidError('player {}: raise must be less than maxraise {}'.format(self.player_id, self.stack))
         move_tuple = ('raise', raise_amount)
       elif action_idx == Player.CALL:
         move_tuple = ('call', tocall)
-      elif choice == Player.FOLD:
+      elif action_idx == Player.FOLD:
         move_tuple = ('fold', -1)
       else:
-        raise error.Error('invalid action ({}) must be raise (2), call (1), or fold (3)'.format(action_idx))
+        self.errors -= 1
+        raise ActionError('player {}: invalid action ({}) must be raise (2), call (1), or fold (3)'.format(self.player_id, action_idx))
     return move_tuple
